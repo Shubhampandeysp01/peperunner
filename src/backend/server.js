2a09c68d858573bc773ada47cb0732013cddad39
Shubhampandeysp01 <Shubhampandeysp01>
2024-05-09 01:35:06 +0530

    checking

--- src/backend/server.js
+++ src/backend/server.js
@@ -91,5 +91,5 @@ app.get('/leaderboard', async (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    // console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
