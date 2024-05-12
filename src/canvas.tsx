3ced9689cd66b749ca5ebe287cfbf9dde430d6ff
Shubhampandeysp01 <Shubhampandeysp01>
2024-05-13 03:22:03 +0530

    canvas styles

--- src/canvas.tsx
+++ src/canvas.tsx
@@ -1,7 +1,19 @@
+import React from 'react';
+import './App.css'; // Import the CSS file
+
 export function Canvas() {
   return (
     <>
-      <canvas id="game" width={1000} height={1000} style={{ border: "15px solid blue", borderRadius: "10px" }}></canvas>
+      <canvas 
+        id="game" 
+        width={1000} 
+        height={1000} 
+        className="canvas-border" 
+        style={{ 
+          border: "15px solid blue",
+          borderRadius: "12px"
+        }}
+      ></canvas>
     </>
   );
 }
