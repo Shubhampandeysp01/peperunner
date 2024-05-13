5975e158999d56f658dabe5eb12a41dd174fe95c
Shubhampandeysp01 <Shubhampandeysp01>
2024-05-13 22:08:05 +0530

    Removed warning

--- src/app.tsx
+++ src/app.tsx
@@ -5,7 +5,6 @@ import { Canvas } from 'canvas';
 import { DEFAULT_STATE, GlobalContext } from 'contexts/global';
 import {Game} from 'game';
 import React, { useState } from 'react';
-import { useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import MyApp from 'updateLeaderboardWithPublicKey';
 import LeaderboardTable from 'LeaderboardTable';
@@ -18,7 +17,6 @@ import './App.css';
 export const App: React.FC = () => {
   const [state, setState] = useState(DEFAULT_STATE.state);
   const [showStartPage, setShowStartPage] = useState(true);
-  const {connected} = useWallet();
 
   const handleStart = () => {
     // Handle start button click
