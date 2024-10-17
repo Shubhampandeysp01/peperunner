88034d9523c64763a303aec57268f8a1e56ee0e6
Shubhampandeysp01 <44943467+Shubhampandeysp01@users.noreply.github.com>
2024-10-17 21:39:05 +0530

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
 // Connection string provided by Vercel
-const connectionString = "postgres://default:GNPaX5WmRh9u@ep-square-meadow-a488ghyx.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
+const connectionString = "postgres://default:cu6EKkNGC0Xg@ep-divine-snowflake-a4q9tyo6.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
 
 // Create a pool
 const pool = new Pool({
